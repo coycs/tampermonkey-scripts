afcd1c19f56c3d82476fbc2288179cc2877e0244
coycs <coycs@qq.com>
2022-05-01 11:38:11 +0800

    webhook测试

--- Hypothesis自定义标注颜色和透明度.js
+++ Hypothesis自定义标注颜色和透明度.js
@@ -14,8 +14,8 @@
 
 (function () {
   "use strict";
-  // webhook同步测试1
-  console.log("webhook同步测试1");
+  // webhook同步测试2
+  console.log("webhook同步测试2");
   window.onload = function () {
     let color, transparency, font_color;
     color = getValue("H_color");
